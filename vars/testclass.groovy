0a567dc4c0dadc263c25f657601163c58dd7ac7c
Lin Ru <Lin.Ru@msn.com>
2019-02-17 23:52:00 +0800

    test

--- vars/testclass.groovy
+++ vars/testclass.groovy
@@ -5,12 +5,10 @@ import me.rulin.ci.Docker
 def call(args) {
     docker_name = args
     if (args == null || (args instanceof String && args.trim().isEmpty())) {
-        docker_name = 'test' //Docker.DOCKER_NAME 
+        docker_name = Docker.DOCKER_NAME 
     }
 
     echo "The name is: " + docker_name
 
-    Docker.Version
+    Docker.Version()
 }
-
-return this
