6d470f4d5ca99190f8752f7e1c14774ce9873683
Lin Ru <Lin.Ru@msn.com>
2019-02-18 14:12:08 +0800

    add env to settings

--- settings.groovy
+++ settings.groovy
@@ -24,99 +24,98 @@ catch (e) {
     log.warning "Requeire 'build user vars' plugin, see https://plugins.jenkins.io/build-user-vars-plugin for more information"
 }
 
-WEB_ROOT            = "/data/web/"
+env.WEB_ROOT            = "/data/web/"
 
 // GIT
-GIT_COMMIT_ID_DISPLAY_LEN = 6
+env.GIT_COMMIT_ID_DISPLAY_LEN = 6
 
 // Stage & Node
 env.STAGE_PRE_PROCESS   = "master"
 env.STAGE_GIT           = ""
-STAGE_BUILD         = 
-STAGE_DOCKER        =
-STAGE_TEST          =
-STAGE_K8S           = 
+env.STAGE_BUILD         = 
+env.STAGE_DOCKER        =
+env.STAGE_TEST          =
+env.STAGE_K8S           = 
 
 // TRIGGERS
-SKIP_BUILD          = false
-SKIP_DOCKER         = false
-SKIP_GIT            = false
-SKIP_SONAR          = false
-SKIP_K8S            = false     // kubernetes
-SKIP_HPA            = false     // kubernetes HPA
-SKIP_CRONJOB        = false     // kubernetes CronJob
-SKIP_JOB            = false     // kubernetes Job
+env.SKIP_BUILD          = false
+env.SKIP_DOCKER         = false
+env.SKIP_GIT            = false
+env.SKIP_SONAR          = false
+env.SKIP_K8S            = false     // kubernetes
+env.SKIP_HPA            = false     // kubernetes HPA
+env.SKIP_CRONJOB        = false     // kubernetes CronJob
+env.SKIP_JOB            = false     // kubernetes Job
 
 ///////////////////////////////////////////////////////
 // BUILD SETTINGS                                    //
 ///////////////////////////////////////////////////////
 // Common
-BUILD_DIR           = "."
-BUILD_COMMAND       = ""
-BUILD_LEGACY        = false
+env.BUILD_DIR           = "."
+env.BUILD_COMMAND       = ""
+env.BUILD_LEGACY        = false
 
 // Golang
 env.GOROOT          = "/usr/lib/go"
 env.GOPATH          =  WORKSPACE
 
 // Maven
-MAVEN_CMD           = "mvn"
-MAVEN_OPTS          = "-U clean -Dmaven.test.skip=true package dependency:tree"
+env.MAVEN_CMD           = "mvn"
+env.MAVEN_OPTS          = "-U clean -Dmaven.test.skip=true package dependency:tree"
 
 // NodeJS
-NPM_REGISTRY        = "https://registry.npm.taobao.org/"
-NPM_INSTALL         = "npm -i " + NPM_REGISTRY
-NPM_I               =  NPM_INSTALL
+env.NPM_REGISTRY        = "https://registry.npm.taobao.org/"
+env.NPM_INSTALL         = "npm -i " + NPM_REGISTRY
+env.NPM_I               =  NPM_INSTALL
 
 // PHP (Composer)
-PHP_COMPOSER_CMD    = "composer install"
+env.PHP_COMPOSER_CMD    = "composer install"
 
 // OSS
-OSS_ENDPOINT        = ""
-OSS_OPTIONS         = "-rf"
+env.OSS_ENDPOINT        = ""
+env.OSS_OPTIONS         = "-rf"
 
 ///////////////////////////////////////////////////////
 // Docker                                            //
 ///////////////////////////////////////////////////////
-DOCKER_REGISTRY                 = ""
-DOCKER_REGISTRY_USER            = ""
+env.DOCKER_REGISTRY                 = ""
+env.DOCKER_REGISTRY_USER            = ""
 // Saved in the Jenkins credentials with name 'Docker-Registry'
 env.DOCKER_REGISTRY_PASSWORD        = credentials('Docker-Registry')
-DOCKER_IMAGE_NAME               = ""
-DOCKER_IMAGE_TAG                = ""
-DOCKER_IMAGE_BUILD_TIMEOUT      = 300
-DOCKER_IMAGE_PUSH_TIMEOUT       = 300
-DOCKER_IMAGE_BUILD_OPTIONS      = ""
+env.DOCKER_IMAGE_NAME               = ""
+env.DOCKER_IMAGE_TAG                = ""
+env.DOCKER_IMAGE_BUILD_TIMEOUT      = 300
+env.DOCKER_IMAGE_PUSH_TIMEOUT       = 300
+env.DOCKER_IMAGE_BUILD_OPTIONS      = ""
 
-DOCKERIGNORE_FILE               = ".dockerignore"
+env.DOCKERIGNORE_FILE               = ".dockerignore"
 
 // Dockerfiles dir
-DOCKERFILES                     = "Dockerfiles"
+env.DOCKERFILES                     = "Dockerfiles"
 
-DOCKERFILE_TEMPLATE_LIST        = []
+env.DOCKERFILE_TEMPLATE_LIST        = []
 
 ///////////////////////////////////////////////////////
 // kubernetes                                        //
 ///////////////////////////////////////////////////////
-K8S_LIMITS_CPU      = "500m"
-K8S_LIMITS_MEMORY   = "512Mi"
-K8S_REQUESTS_CPU    = "500m"
-K8S_REQUESTS_MEMORY = "512Mi"
+env.K8S_LIMITS_CPU      = "500m"
+env.K8S_LIMITS_MEMORY   = "512Mi"
+env.K8S_REQUESTS_CPU    = "500m"
+env.K8S_REQUESTS_MEMORY = "512Mi"
 
-K8S_RUN_USER_ID                 = 
-K8S_NAMESPACE                   = 
-K8S_REPLICAS                    = 
+env.K8S_RUN_USER_ID                 = 
+env.K8S_NAMESPACE                   = 
+env.K8S_REPLICAS                    = 
 
-K8S_AFFINITY_PROVIDER           = 
-K8S_AFFINITY_ROLE               =
-K8S_AFFINITY_REGION             = 
+env.K8S_AFFINITY_PROVIDER           = 
+env.K8S_AFFINITY_ROLE               =
+env.K8S_AFFINITY_REGION             = 
 
-K8S_IMAGE_PULL_POLICY           = "IfNotPresent"
-K8S_REVISION_HISTORY_LIMIT      = 5
-K8S_STRATEGY_MAX_SURGE          = "10%"
-K8S_STRATEGY_MAX_UNAVAILABLE    = "10%"
-K8S_GRACE_PERIOD_SECONDS        = 60  // Sec
-K8S_MIN_READY_SECONDS           = 5   // Sec
+env.K8S_IMAGE_PULL_POLICY           = "IfNotPresent"
+env.K8S_REVISION_HISTORY_LIMIT      = 5
+env.K8S_STRATEGY_MAX_SURGE          = "10%"
+env.K8S_STRATEGY_MAX_UNAVAILABLE    = "10%"
+env.K8S_GRACE_PERIOD_SECONDS        = 60  // Sec
+env.K8S_MIN_READY_SECONDS           = 5   // Sec
 
-K8S_ALLOWED_COMMADS             = ["apply", "create", "delete", "get"]
-K8S_ALLOWED_COMMADS            += ["autoscale"]
+env.K8S_ALLOWED_COMMADS             = ["apply", "create", "delete", "get", "autoscale"]
