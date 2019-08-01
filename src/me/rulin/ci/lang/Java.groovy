7fd089aed0711914ad7d0380ab16972e4ad8e3f1
Lin Ru <rulin@Lins-MBP-13.local>
2019-08-02 01:13:37 +0800

    add Java

--- src/me/rulin/ci/lang/Java.groovy
+++ src/me/rulin/ci/lang/Java.groovy
@@ -9,3 +9,31 @@
 
 package me.rulin.ci.lang
 
+def initBuild(String b_file){
+    log.i "Preparing Java build"
+
+    private  bc = Config.data['build_command']
+    private  bo = Config.data['build_options']
+    private cmd = bc + bo
+
+    try {
+        if (fileExists(b_file)){
+            log.i "Build with command: " + bc + ", options: " + bo
+            sh(cmd)
+        }
+        else {
+            log.e "File not found: '" + b_file + "', aborted"
+        }
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+def mavenBuild(){
+    initBuild('pom.xml')
+}
+
+def gradleBuild(){
+    initBuild('build.gradle')
+}
