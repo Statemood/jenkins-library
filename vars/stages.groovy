e587242ced24914700ff891e96082caaa237335a
Lin Ru <rulin@Lins-MBP-13.local>
2019-08-02 00:06:53 +0800

    update stages

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -8,91 +8,65 @@
 */
 
 import me.rulin.ci.Git
-import me.rulin.ci.SonarQube
+import me.rulin.ci.SonarQubeScanner
 
 def call(Map args = [:]) {
     Config.data  += args
 
     loadSettings()
     preProcess()
-    git()
+    gitClone()
     if (Config.data['lang'] == "java") {
         compile(3)
-        //sonar(4)
         test(4)
     }
     else {
-        //sonar()
         compile()
         test()
     }
 }
 
 def preProcess(stage_id=1) {
-    node(STAGE_PRE_PROCESS) {
-        stage("Stage $stage_id: Pre-Process") {
-            // Set default info
+    stage("Stage $stage_id: Pre-Process") {
+        // Set default info
+        // Set build info
+        // Check parameters
 
-            // Set build info
-
-            // Check parameters
-
-            echo "Stage Pre-Process OK"
-        }
+        log.i "Stage Pre-Process OK"
     }
 }
 
-def git(stage_id=2) {
-    node(STAGE_GIT) {
-        stage("Stage $stage_id: Git Checkout") {
-            git = new Git()
+def gitClone(stage_id=2) {
+    stage("Stage $stage_id: Git Clone") {
+        gitco = new Git()
 
-            git.checkout(Config.data['repo'], Config.data['revision'])
-        }
+        gitco.checkout(Config.data['repo'], Config.data['revision'])
     }
 }
 
 def sonar(stage_id=3) {
-    node(STAGE_SONAR) {
-        stage("Stage $stage_id: SonarQube Scanner") {
-            sonar = new SonarQube()
-            sonar.scan()
-        }
+    stage("Stage $stage_id: SonarQube Scanner") {
+        sonar = new SonarQubeScanner()
+        sonar.scan()
     }
 }
 
 def compile(stage_id=4) {
-    node(STAGE_BUILD) {
-        stage("Stage $stage_id: Build Code") {
-            build.controller()
-        }
+    stage("Stage $stage_id: Build Code") {
+        build.controller()
     }
 }
 
 def test(stage_id=5) {
-    node(STAGE_TEST) {
-        stage("Stage $stage_id: Test") {
-            test_cmd = Config.data['test_cmd']
-            if (test_cmd) {
-                log.info "Test by command: $test_cmd"
+    stage("Stage $stage_id: Test") {
+        test_cmd = Config.data['test_cmd']
+        if (test_cmd) {
+            log.i "Test by command: $test_cmd"
 
-                sh(test_cmd)
-            }
-            return
+            sh(test_cmd)
         }
+        return
     }
-}
-
-node(STAGE_DOCKER) {
-    docker.controller()
-}
-
-node(STAGE_KUBERNETES) {
-
-}
-
-node(STAGE_POST_PROCESS) {
-
 }
 
 return this
