f1cd0304c871ebfabf2a9f9845758571cc91a9e5
Lin Ru <rulin@Lins-MBP-15.local>
2019-08-19 07:16:00 +0800

    jp: set default value for git

--- vars/set.groovy
+++ vars/set.groovy
@@ -8,6 +8,9 @@
 */
 
 def call(Map args = [:]) {
+
+    Config.data['repo']         = args.containsKey('repo')        ?: null
+    Config.data['revision']     = args.containsKey('revision')    ?: GIT_REVISION
     Config.data['lang']         = args.containsKey('lang')        ?: "java"
 
     Config.data += args 
