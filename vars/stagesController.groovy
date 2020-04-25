e7ae17f096e7ad830cb82805e2b958132eacead2
Lin Ru <Lin.Ru@msn.com>
2020-04-26 07:38:50 +0800

    test git commit id

--- vars/stagesController.groovy
+++ vars/stagesController.groovy
@@ -61,7 +61,7 @@ def dockerStage(){
     def private  di = new DockerImage()
     def private git = new Git()
 
-    def private tag = GIT_REVISION + '-' + git.commitIDShort(8)
+    def private tag = GIT_REVISION + '-' + git.commitIDShort()
 
     def image = DOCKER_REGISTRY + '/' + PROJECT_NAME + '/' + APP_NAME + ':' + tag 
     
