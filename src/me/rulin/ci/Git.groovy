d04c59a9867186382aa617a86f3a654172b441c1
Lin Ru <Lin.Ru@msn.com>
2020-04-26 23:16:10 +0800

    test gid

--- src/me/rulin/ci/Git.groovy
+++ src/me/rulin/ci/Git.groovy
@@ -33,8 +33,8 @@ def clone(repo, revision, cid='DefaultGitSCMCredentialsID') {
 }
 
 def commitID(){
-    //println "git rev-parse HEAD".execute().text
-    sh("git rev-parse HEAD")
+    println "git rev-parse HEAD".execute().text
+    //sh("git rev-parse HEAD")
 }
 
 def commitIDShort(int len=GIT_COMMIT_ID_DISPLAY_LEN){
