302cf10df57fd0cb31e923f91d209bcc7d5e9625
Lin Ru <Lin.Ru@msn.com>
2020-05-02 15:49:51 +0800

    test docker class

--- src/me/rulin/docker/Image.groovy
+++ src/me/rulin/docker/Image.groovy
@@ -11,75 +11,77 @@ package me.rulin.docker
 
 import  me.rulin.docker.Command
 
-def private build(String image_name) {
-    def cmd = new Command()
-    check.file('Dockerfile')
-    try {
-        
-        log.info "Build image: " + image_name
+class Image {
+    def private build(String image_name) {
+        def cmd = new Command()
+        check.file('Dockerfile')
+        try {
+            
+            log.info "Build image: " + image_name
 
-        timeout(time: DOCKER_IMAGE_BUILD_TIMEOUT, unit: 'SECONDS') {
-            cmd.exec("build $DOCKER_IMAGE_BUILD_OPTIONS -t $image_name .")
-        }
-    }
-    catch (e) {
-        println "Error occurred during build image"
-        throw e
-    }
-}
-
-def private version(){
-    def cmd = new Command()
-    cmd.exec("version")
-}
-
-def private images(){
-    def public cmd = new Command()
-    cmd.exec("images")
-}
-
-def private push(String image_name){
-    def public cmd = new Command()
-    try {
-        log.info "Push image " + image_name
-
-        timeout(time: DOCKER_IMAGE_PUSH_TIMEOUT, unit: 'SECONDS') {
-            cmd.exec("push $image_name")
-        }
-    }
-    catch (e) {
-        println "Error occurred during push image"
-        throw e
-    }
-}
-
-def login(String reg=DOCKER_REGISTRY, String opt=null){
-    def public cmd = new Command()
-    if(reg){
-        private r = ""
-    }
-    try {
-        log.i "Login to Docker Registry " + reg
-
-        timeout(time: DOCKER_IMAGE_PUSH_TIMEOUT, unit: 'SECONDS') {
-            withCredentials([
-                usernamePassword(
-                    credentialsId: 'Docker-Registry',
-                    passwordVariable: 'registry_password',
-                    usernameVariable: 'registry_username'
-                )
-            ]) {
-                cmd.exec("login -u $registry_username -p $registry_password $reg")
+            timeout(time: DOCKER_IMAGE_BUILD_TIMEOUT, unit: 'SECONDS') {
+                cmd.exec("build $DOCKER_IMAGE_BUILD_OPTIONS -t $image_name .")
             }
         }
+        catch (e) {
+            println "Error occurred during build image"
+            throw e
+        }
     }
-    catch (e) {
-        println "Error occurred during push image"
-        throw e
-    }
-}
 
-def logout(){
-    def public cmd = new Command()
-    cmd.exec("logout")
+    def private version(){
+        def cmd = new Command()
+        cmd.exec("version")
+    }
+
+    def private images(){
+        def public cmd = new Command()
+        cmd.exec("images")
+    }
+
+    def private push(String image_name){
+        def public cmd = new Command()
+        try {
+            log.info "Push image " + image_name
+
+            timeout(time: DOCKER_IMAGE_PUSH_TIMEOUT, unit: 'SECONDS') {
+                cmd.exec("push $image_name")
+            }
+        }
+        catch (e) {
+            println "Error occurred during push image"
+            throw e
+        }
+    }
+
+    def login(String reg=DOCKER_REGISTRY, String opt=null){
+        def public cmd = new Command()
+        if(reg){
+            private r = ""
+        }
+        try {
+            log.i "Login to Docker Registry " + reg
+
+            timeout(time: DOCKER_IMAGE_PUSH_TIMEOUT, unit: 'SECONDS') {
+                withCredentials([
+                    usernamePassword(
+                        credentialsId: 'Docker-Registry',
+                        passwordVariable: 'registry_password',
+                        usernameVariable: 'registry_username'
+                    )
+                ]) {
+                    cmd.exec("login -u $registry_username -p $registry_password $reg")
+                }
+            }
+        }
+        catch (e) {
+            println "Error occurred during push image"
+            throw e
+        }
+    }
+
+    def logout(){
+        def public cmd = new Command()
+        cmd.exec("logout")
+    }
 }
