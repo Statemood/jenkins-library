9e0e9584c5cb70b8bc2de4d2c4bdb79e5aec0b94
Lin Ru <rulin@fury.local>
2021-01-27 11:34:00 +0800

    update k8s, fix typo

--- src/me/rulin/k8s/Kubernetes.groovy
+++ src/me/rulin/k8s/Kubernetes.groovy
@@ -14,23 +14,23 @@ package me.rulin.k8s
 
 def updateYaml(String yaml_file="k8s.yaml"){
     try {
-        def private data = readYaml file: yaml_file
-        def c   = data.spec.template.spec.containers[0]
+        def private yaml_data = readYaml file: yaml_file
+        def c   = yaml_data.spec.template.spec.containers[0]
         def res = c.resources
-        println data
-                
-        data.metadata.name  = APP_NAME
-        c.image             = DOCKER_IMAGE
-        c.imagePullPolicy   = "Always"
-        res.requests.cpu    = K8S_REQUESTS_CPU
-        res.requests.memory = K8S_REQUESTS_MEMORY
-        res.limits.cpu      = K8S_LIMITS_CPU
-        res.limits.memory   = K8S_LIMITS_MEMORY
+        println yaml_data
+
+        yaml_data.metadata.name = APP_NAME
+        c.image                 = DOCKER_IMAGE
+        c.imagePullPolicy       = "Always"
+        res.requests.cpu        = K8S_REQUESTS_CPU
+        res.requests.memory     = K8S_REQUESTS_MEMORY
+        res.limits.cpu          = K8S_LIMITS_CPU
+        res.limits.memory       = K8S_LIMITS_MEMORY
         
 
         //data.replaceAll("APP_NAME", APP_NAME)
 
-        println data
+        println yaml_data
 
         writeYaml file: yaml_file, data: yaml_data, overwrite: true
     }
