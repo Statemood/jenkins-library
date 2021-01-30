75fd3acadb7dd99239a1f8d0a3ab30a613569d87
Lin Ru <rulin@fury.local>
2021-01-31 02:48:40 +0800

    test yaml, env

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -46,7 +46,7 @@ def call(Map config=[:]){
                         
                         fc.getConfig()
                         */
-                        stageEntryPoint(config)
+                        stageController.stageEntryPoint(config)
                     }
                 }
             }
