79aa55acbcfc7a21e2f140b36883955944d1a7fb
Lin Ru <rulin@fury.local>
2021-02-02 15:18:49 +0800

    update controller

--- vars/controller.groovy
+++ vars/controller.groovy
@@ -24,27 +24,6 @@ def entry(Map args = [:]) {
 
     settings.merge(args)
 
-<<<<<<< HEAD
-    log.i 'Using workspace: ' + Config.data.base_dir
-=======
-    if(!ACTION) { ACTION = "deploy" }   
-
-    Config.data['repo']             = args.containsKey('repo')              ?: null
-    Config.data['revision']         = args.containsKey('revision')          ?: GIT_REVISION
-    Config.data['language']         = args.containsKey('language')          ?: "java"
-    Config.data['action']           = ACTION
-    Config.data['build.user']       = BUILD_USER
-    Config.data['env']              = ENVIRONMENT
-    Config.data['credentials.id']   = args.containsKey('credentials.id')    ?: "DefaultGitSCMCredentialsID"
-    
-    Config.data += args 
-
-    println Config.data 
-    println args 
-
-    log.i "Using workspace: " + FIRST_DIR
->>>>>>> master
-
     preProcess()
     codeClone()
     codeBuild()
