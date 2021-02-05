09a00a818c7f43e0755d459846f5f6e2ce99c865
Lin Ru <rulin@fury.local>
2021-02-05 18:50:44 +0800

    fix typo

--- src/me/rulin/ci/SonarQube.groovy
+++ src/me/rulin/ci/SonarQube.groovy
@@ -12,29 +12,27 @@ package me.rulin.ci
 def scanner(String o='') {
     try {
         log.i 'Preparing SonarQube Scanner'
+        withSonarQubeEnv(){
+            withCredentials([
+                usernamePassword(
+                    credentialsId: 'Sonar-Account',
+                    passwordVariable: 'sonar_p',
+                    usernameVariable: 'sonar_u')])
+            {
+                private ssc_u = ' -Dsonar.login='            + sonar_u
+                private ssc_p = ' -Dsonar.password='         + sonar_p
+                private ssc_k = ' -Dsonar.projectKey='       + Config.data.base_name
+                private ssc_n = ' -Dsonar.projectName='      + Config.data.base_name
+                private ssc_v = ' -Dsonar.projectVersion='   + Config.data.git_revision
+                private ssc_d = ' -Dsonar.projectBaseDir=.'
+                private ssc_l = ' -Dsonar.language='         + Config.data.build_language
+                private ssc_s = ' -Dsonar.sources=.'
+                private ssc_b = ' -Dsonar.java.binaries=.'
+                
+                sonar_opts  = ssc_u + ssc_p + ssc_k + ssc_n + ssc_v + ssc_d + ssc_l + ssc_s + ssc_b
+                sonar_opts += o
+                sonar_exec  = 'sonar-scanner ' + sonar_opts  
 
-        withCredentials([
-            usernamePassword(
-                credentialsId: 'Sonar-Account',
-                passwordVariable: 'sonar_p',
-                usernameVariable: 'sonar_u')])
-        {
-            private ssc_u = ' -Dsonar.login='            + sonar_u
-            private ssc_p = ' -Dsonar.password='         + sonar_p
-            private ssc_k = ' -Dsonar.projectKey='       + Config.data.base_name
-            private ssc_n = ' -Dsonar.projectName='      + Config.data.base_name
-            private ssc_v = ' -Dsonar.projectVersion='   + Config.data.git_revision
-            private ssc_d = ' -Dsonar.projectBaseDir=.'
-            private ssc_l = ' -Dsonar.language='         + Config.data.build_language
-            private ssc_s = ' -Dsonar.sources=.'
-            private ssc_b = ' -Dsonar.java.binaries=.'
-            
-            sonar_opts  = ssc_u + ssc_p + ssc_k + ssc_n + ssc_v + ssc_d + ssc_l + ssc_s + ssc_b
-            sonar_opts += o
-            sonar_exec  = 'sonar-scanner ' + sonar_opts  
-
-            // TODO: try another way
-            withSonarQubeEnv('SonarQube'){
                 sh(sonar_exec)
             }
         }
