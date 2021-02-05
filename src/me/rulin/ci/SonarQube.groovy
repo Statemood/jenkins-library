7374c53f988d0c809f4661edcacb5df671d517a5
Lin Ru <rulin@fury.local>
2021-02-05 18:59:03 +0800

    test sonarqube gate

--- src/me/rulin/ci/SonarQube.groovy
+++ src/me/rulin/ci/SonarQube.groovy
@@ -36,6 +36,13 @@ def scanner(String o='') {
                 sh(sonar_exec)
             }
         }
+
+        timeout(time: 1, unit: 'MINUTES') { // Just in case something goes wrong, pipeline will be killed after a timeout
+            def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+            if (qg.status != 'OK') {
+                error "Pipeline aborted due to quality gate failure: ${qg.status}"
+            }
+        }
     }
     catch (e) {
         log.e 'Failed with Sonar Scanner'
