73f3feb3241db530ff6ce57614351c4408e75fce
Lin Ru <rulin@fury.local>
2021-04-01 22:38:08 +0800

    update controoler

--- vars/controller.groovy
+++ vars/controller.groovy
@@ -7,15 +7,17 @@
    ##################################################
 */
 
-import me.rulin.cd.Server
-import me.rulin.ci.Git
-import me.rulin.ci.Language
-import me.rulin.ci.SonarQube
-import me.rulin.docker.Docker
-import me.rulin.kubernetes.Command
-import me.rulin.kubernetes.Yaml
+import io.rulin.artifact.Harbor
+import io.rulin.cd.Server
+import io.rulin.cd.WeixinMiniProgram
+import io.rulin.ci.Git
+import io.rulin.ci.Language
+import io.rulin.ci.SonarQube
+import io.rulin.docker.Docker
+import io.rulin.kubernetes.Command
+import io.rulin.kubernetes.Yaml
 
-def entry(Map args = [:]) {
+def entry() {
     /*
     Order:
         1. Local Settings
@@ -23,14 +25,45 @@ def entry(Map args = [:]) {
         3. Config
     */
 
-    settings.merge(args)
-
     preProcess()
     codeClone()
 
     def private scan_after_build = ['java']
+    def                        d = Config.data
+    def private   none_container = false
+    def private             lang = Config.data.build_language.toLowerCase()
+    def private          version = Config.data.build_language_version
+    def private        node_name = lang
 
-    if(Config.data.build_language in scan_after_build) {
+    if(version != 0){ node_name = lang + '-' + version }
+
+    d.build_node_name = node_name
+
+    if(d.hosted_by.toLowerCase() != 'container' && DEPLOYMENT_MODE.toLowerCase() == 'legacy' ){
+        none_container      = true 
+    } 
+    else {
+        d.docker_img_tag    = d.git_revision    + '-' + d.git_commit_id[0..8]
+        d.docker_img_name   = d.docker_registry + '/' + d.base_project + '/' + d.base_name
+        d.docker_img        = d.docker_img_name + ':' + d.docker_img_tag
+    }
+
+    if(d.hosted_by.toLowerCase() == 'mmp'){
+        none_container      = true
+    }
+
+    if(isImageExist()){
+        log.a 'Artifact version ' + d.docker_img_tag + ' already exist, skip build.'
+        d.skip_build        = true
+        d.skip_docker       = true
+    }
+
+    if(none_container){
+        d.skip_docker       = true 
+        d.skip_k8s          = true
+    }
+
+    if(d.build_language in scan_after_build) {
         codeBuild()
     
         parallel (
@@ -49,12 +82,15 @@ def entry(Map args = [:]) {
         )
     }
 
-    doKubernetes()
+    if(!d.skip_deploy){
+        doKubernetes()
+        deploy2MMP()
+    }
 }
 
 // Set build info
 def currentBuildInfo(){
-    def private name = BUILD_NUMBER + '-' + Config.data.base_env
+    def private name = BUILD_NUMBER + '-' + Config.data.base_env.toUpperCase()
     def private desc = Config.data.build_user + ' ' + Config.data.base_action + ' ' + Config.data.git_revision
 
     currentBuild.displayName = name 
@@ -63,12 +99,15 @@ def currentBuildInfo(){
 
 def preProcess() {
     stage('Pre-Process') {
-        node(STAGE_PRE_PROCESS) {
+        node(Config.data.stage_pre_process) {
             dir(Config.data.base_dir) {
                 // Set default info
                 // Set build info
                 // Check parameters
                 currentBuildInfo()
+                if(Config.data.check_permission){
+                    check.permission(Config.data.build_userid)
+                }
                 log.i 'Stage Pre-Process OK'
             }
         }
@@ -77,24 +116,13 @@ def preProcess() {
 
 def codeClone() {
     stage('Git Clone') {
-        if(!Config.data.skip_git){
-            node(STAGE_GIT) {
-                dir(Config.data.base_dir) {
-                    try {
-                        def private revision = Config.data.git_revision
-                        def private     repo = Config.data.git_repo
-
-                        log.i 'Git clone ' + revision + ' from ' + repo
-
-                        git credentialsId: Config.data.git_credentials,
-                            branch: revision,
-                            url: repo
-
-                        metis.getGetCommitID()
-                    } catch (e) {
-                        log.e 'Ops! Error occurred during git checkout'
-                        throw e
-                    }
+        def private d = Config.data
+        def private g = new Git()
+        if(!d.skip_git){
+            node(d.stage_git) {
+                dir(d.base_dir) {
+                    g.co(d.git_revision, d.git_repo, d.git_credentials_clone)
+                    metis.getGetCommitID()
                 }
             }
         }
@@ -107,10 +135,10 @@ def codeClone() {
 def sonarScan() {
     stage('SonarQube Scanner') {
         if(!Config.data.skip_sonar){
-            node(STAGE_SONAR) {
+            node(Config.data.build_node_name) {
                 dir(Config.data.base_dir) {
                     def sonar = new SonarQube()
-                    sonar.scanner()
+                    sonar.scanner(Config.data.sonar_scanner)
                 }
             }
         }
@@ -122,7 +150,7 @@ def sonarScan() {
 
 def qualityGate(){
     stage("Quality Gate") {
-        node(STAGE_SONAR) {
+        node(Config.data.build_node_name) {
             timeout(time: 30, unit: 'MINUTES') {
                 dir(Config.data.base_dir) {
                     waitForQualityGate abortPipeline: true
@@ -132,20 +160,42 @@ def qualityGate(){
     }
 }
 
+def isImageExist(){
+    def private harbor = new Harbor()
+    def private  found = false
+    def private      d = Config.data
+    def private    tag = d.git_revision + '-' + d.git_commit_id[0..8]
+    def private    res = harbor.artifactInfo(
+                            tag,
+                            d.docker_registry_basic_auth,
+                            d.docker_registry,
+                            d.base_project,
+                            d.base_name
+                        )
+                        
+    res.each{
+        t = it['tags']
+        if(t != 'null'){
+            t.each{lists->
+                if(lists['name'] == tag){
+                    found = true
+                    return true 
+                }
+                return false
+            }
+        }
+    }
+    return found
+}
+
 def codeBuild() {
-    stage('Build Code') {
-        def private      lang = Config.data.build_language.toLowerCase()
-        def private   version = Config.data.build_language_version
-        def private node_name = lang
-
-        if(version != 0){ node_name = lang + '-' + version }
-
-        Config.data.build_node_name = node_name
-
-        node(node_name) {
-            dir(Config.data.base_dir) {
-                def language = new Language()
-                language.seletor(lang)
+    if(!Config.data.skip_build){
+        stage('Build Code') {
+            node(Config.data.build_node_name) {
+                dir(Config.data.base_dir) {
+                    def language = new Language()
+                    language.seletor(Config.data.build_language)
+                }
             }
         }
     }
@@ -167,32 +217,28 @@ def codeTest() {
 }
 
 def doDocker(){
-    if(DEPLOYMENT_MODE == 'Legacy') { return }
+    def private d = Config.data
+    if(DEPLOYMENT_MODE == 'Legacy' || d.skip_docker) { return }
     stage('Docker') {
-        node(STAGE_DOCKER) {
-            dir(Config.data.base_dir) {
-                def private  docker = new Docker()
-                def private     cfg = Config.data
-                Config.data.docker_img_tag  = cfg.git_revision    + '-' + cfg.git_commit_id[0..8]
-                Config.data.docker_img_name = DOCKER_REGISTRY     + '/' + cfg.base_project + '/' + cfg.base_name
-                Config.data.docker_img      = cfg.docker_img_name + ':' + cfg.docker_img_tag
-
+        node(d.stage_docker) {
+            dir(d.base_dir) {
+                def private docker = new Docker()
                 parallel (
                     'Generate Dockerfile': { docker.genDockerfile() },
                     'Login to Registry'  : { docker.login() }
                 )
                 
-                docker.build(cfg.docker_img)
-                docker.push(cfg.docker_img)
+                docker.build(d.docker_img)
+                docker.push(d.docker_img)
             }
         }
     }
 }
 
 def doKubernetes(){
-    if(DEPLOYMENT_MODE == 'Legacy') { return }
+    if(DEPLOYMENT_MODE == 'Legacy' || Config.data.skip_k8s) { return }
     stage('Kubernetes') {
-        node(STAGE_K8S) {
+        node(Config.data.stage_k8s) {
             dir(Config.data.base_dir) {
                 def cmd     = new Command()
                 def gen     = new Yaml()
@@ -222,6 +268,36 @@ def doServer(List th=[]){
 
     for(host in th){
         server.upload()
+    }
+}
+
+def deploy2MMP(String dist='./dist/'){
+    if(Config.data.hosted_by == 'mmp' && Config.data.build_language == 'nodejs'){
+        stage('Deploy to MiniProgram'){
+            node(Config.data.build_node_name){
+                def private project_config = 'dist/project.config.json'
+
+                check.file(project_config)
+
+                def private  cfg = readJSON file: project_config
+                def private  mmp = new WeixinMiniProgram()
+                def private  cid = 'Private-Key-' + Config.data.base_name + '-' + Config.data.base_project + '-' + ENVIRONMENT
+                def private desc = 'Upload by '   + Config.data.build_user + ', Jenkins ID #' + BUILD_NUMBER + 
+                                    '. Version '  + Config.data.git_revision + ', commit id: ' + Config.data.git_commit_id + '.'
+
+                withCredentials([file(credentialsId: cid, variable: 'MMP_PRIVATE_KEY')]){
+                    check.file(MMP_PRIVATE_KEY)
+                    mmp.upload(
+                        dist,
+                        MMP_PRIVATE_KEY,
+                        cfg['appid'],
+                        Config.data.git_revision + '.' + ENVIRONMENT.toLowerCase(),
+                        cfg.setting['es6'],
+                        desc
+                    )
+                }
+            }
+        }
     }
 }
 
